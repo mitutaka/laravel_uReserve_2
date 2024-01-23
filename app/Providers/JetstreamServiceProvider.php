e15abcc2f9a764d1d2f6ecd5c3c82b462405173c
hanada <90606895+mitutaka@users.noreply.github.com>
2024-01-23 15:07:58 +0900

    view file

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -5,45 +5,47 @@ namespace App\Providers;
 use App\Actions\Jetstream\DeleteUser;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Jetstream\Jetstream;
+use Laravel\Fortify\Fortify;
 
 class JetstreamServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+    // Fortify::ignoreRoutes(); // Fortifyのルーティングを無効化する方法
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->configurePermissions();
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    $this->configurePermissions();
 
-        Jetstream::deleteUsersUsing(DeleteUser::class);
-    }
+    Jetstream::deleteUsersUsing(DeleteUser::class);
+  }
 
-    /**
-     * Configure the permissions that are available within the application.
-     *
-     * @return void
-     */
-    protected function configurePermissions()
-    {
-        Jetstream::defaultApiTokenPermissions(['read']);
+  /**
+   * Configure the permissions that are available within the application.
+   *
+   * @return void
+   */
+  protected function configurePermissions()
+  {
+    Jetstream::defaultApiTokenPermissions(['read']);
 
-        Jetstream::permissions([
-            'create',
-            'read',
-            'update',
-            'delete',
-        ]);
-    }
+    Jetstream::permissions([
+      'create',
+      'read',
+      'update',
+      'delete',
+    ]);
+  }
 }
