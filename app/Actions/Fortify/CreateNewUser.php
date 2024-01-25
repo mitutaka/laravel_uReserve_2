6d695fd67546cb610bfb372cefa91870796ad7a3
hanada <90606895+mitutaka@users.noreply.github.com>
2024-01-25 22:18:09 +0900

    gate

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -10,27 +10,28 @@ use Laravel\Jetstream\Jetstream;
 
 class CreateNewUser implements CreatesNewUsers
 {
-    use PasswordValidationRules;
+  use PasswordValidationRules;
 
-    /**
-     * Validate and create a newly registered user.
-     *
-     * @param  array  $input
-     * @return \App\Models\User
-     */
-    public function create(array $input)
-    {
-        Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => $this->passwordRules(),
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
-        ])->validate();
+  /**
+   * Validate and create a newly registered user.
+   *
+   * @param  array  $input
+   * @return \App\Models\User
+   */
+  public function create(array $input)
+  {
+    Validator::make($input, [
+      'name' => ['required', 'string', 'max:255'],
+      'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+      'password' => $this->passwordRules(),
+      'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
+    ])->validate();
 
-        return User::create([
-            'name' => $input['name'],
-            'email' => $input['email'],
-            'password' => Hash::make($input['password']),
-        ]);
-    }
+    return User::create([
+      'name' => $input['name'],
+      'email' => $input['email'],
+      'password' => Hash::make($input['password']),
+      'role' => 9
+    ]);
+  }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,24 +7,32 @@ use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-    ];
+  /**
+   * The policy mappings for the application.
+   *
+   * @var array<class-string, class-string>
+   */
+  protected $policies = [
+    // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+  ];
 
-    /**
-     * Register any authentication / authorization services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerPolicies();
+  /**
+   * Register any authentication / authorization services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    $this->registerPolicies();
 
-        //
-    }
+    Gate::define('admin', function ($user) {
+      return $user->role === 1;
+    });
+    Gate::define('manager-higher', function ($user) {
+      return $user->role > 0 && $user->role <= 5;
+    });
+    Gate::define('user-higher', function ($user) {
+      return $user->role > 0 && $user->role <= 9;
+    });
+  }
 }
